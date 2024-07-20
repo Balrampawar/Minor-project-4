3c46f23584204468522bcbf8ce177443bf82e4a2
Balram Pawar <135170120+Balrampawar@users.noreply.github.com>
2024-07-20 14:11:53 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,13 @@
+let playagain = document.querySelector(".playagain");
+
+playagain.addEventListener("click", () => {
+
+//  console.log("Hiiii")
+
+    window.location.reload();
+    window.location.href = 'ModuleTest1.html';
+
+    // console.log("Hiiii  vishal")
+
+    
+});
